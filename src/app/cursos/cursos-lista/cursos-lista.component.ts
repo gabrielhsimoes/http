be3ec1296f6264dfe2514c55cc8679fb38c432f3
Gabriel Simoes <gabrielsimoesy@gmail.com>
2022-04-14 16:02:06 -0400

    requisições http

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, empty, Observable, Subject } from 'rxjs';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 
@@ -14,13 +14,38 @@ export class CursosListaComponent implements OnInit {
   // cursos!: Curso[]
 
   cursos$!: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
+  
 
   constructor(private service: CursosService) { }
 
   ngOnInit(): void {
     // this.service.list().subscribe(dados => this.cursos = dados);
 
-    this.cursos$ = this.service.list();
+    this.onRefresh();
+  }
+
+  onRefresh(){
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return empty();
+      })
+    );
+
+    this.service.list()
+    .pipe(
+      catchError(error => empty())
+    )
+    .subscribe(
+      dados => {
+        console.log(dados);
+      },
+      // error => console.error(error),
+      // () => console.log("Observable completo!")
+    );
   }
 
 }
