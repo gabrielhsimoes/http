f4aab09c703b0eae01637ac3edab4b9ba50fc122
Gabriel Simoes <gabrielsimoesy@gmail.com>
2022-04-14 11:39:03 -0400

    unsubscribe dos componentes

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Curso } from './curso';
-import { tap } from 'rxjs';
+import { delay, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
 
-  private readonly API = 'http://localhost:3000/cursos';
+  private readonly API = `${environment.API}cursos`;
 
   constructor(private http: HttpClient) { }
 
   list(){
     return this.http.get<Curso[]>(this.API)
     .pipe(
+      delay(2000),
       tap(console.log)
     )
   }
